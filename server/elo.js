e24bc5df7c3da34707630477590049b9096db6e1
christos <chrishadjiaslanis@gmail.com>
2018-11-10 14:53:08 +0000

    Removed random logging from elo.js

--- server/elo.js
+++ server/elo.js
@@ -7,6 +7,3 @@ function rating(rWinner, rLoser, k) {
 function winningProbability(rWinner, rLoser) {
 	return 1 / (1 + Math.pow(10, (rLoser - rWinner) / 400));
 }
-
-console.log(rating(1200, 1000, 35));
-console.log(rating(1000, 1200, 35));
